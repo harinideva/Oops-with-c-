7d177b5f9f5fc25160294c08973545112bfe2652
harinideva <130039261+harinideva@users.noreply.github.com>
2024-10-15 19:16:12 +0530

    Add files via upload

---
+++ valid or invalid.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+string str1,str2;
+cout<<"the username1:";
+cin>>str1;
+cout<<"the username2:";
+cin>>str2;
+if(str1==str2){
+	cout<<"it is valid";
+}
+else{
+	cout<<"it is not valid";
+}
+}
